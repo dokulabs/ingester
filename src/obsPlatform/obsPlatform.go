58f91161a290ce8ab837a3326396b9d576760c81
ishanjainn <ishan.jain0099@gmail.com>
2023-12-27 19:48:27 +0530

    First draft of sending metrics and logs to Grafana Cloud

--- src/obsPlatform/obsPlatform.go
+++ src/obsPlatform/obsPlatform.go
@@ -16,7 +16,7 @@ import (
 var (
 	ObservabilityPlatform string       // ObservabilityPlatform contains the information on the platform in use.
 	httpClient            *http.Client // httpClient is the HTTP client used to send data to the Observability Platform.
-	grafanaPostUrl        string       // grafanaPostUrl is the URL used to send data to Grafana Loki.
+	grafanaLokiPostUrl        string       // grafanaPostUrl is the URL used to send data to Grafana Loki.
 )
 
 func Init(cfg config.Configuration) error {
@@ -29,7 +29,7 @@ func Init(cfg config.Configuration) error {
 		// Use strings.TrimPrefix to remove the schemes "http://" and "https://"
 		grafanaLokiUrl = strings.TrimPrefix(grafanaLokiUrl, "http://")
 		grafanaLokiUrl = strings.TrimPrefix(grafanaLokiUrl, "https://")
-		grafanaPostUrl = fmt.Sprintf(
+		grafanaLokiPostUrl = fmt.Sprintf(
 			"https://%s:%s@%s",
 			grafanaLogsUsername,
 			grafanaAccessToken,
@@ -43,61 +43,32 @@ func Init(cfg config.Configuration) error {
 func SendToPlatform(data map[string]interface{}) {
 	switch ObservabilityPlatform {
 	case "GRAFANA":
-		streamData := make(map[string]interface{})
-		fields := []string{
-			"environment",
-			"sourceLanguage",
-			"applicationName",
-			"completionTokens",
-			"promptTokens",
-			"totalTokens",
-			"finishReason",
-			"requestDuration",
-			"usageCost",
-			"model",
-			"prompt",
-			"imageSize",
-			"image",
-			"revisedPrompt",
-			"audioVoice",
-			"finetuneJobId",
-			"finetuneJobStatus",
-		}
+		if data["endpoint"] == "openai.chat.completions" {
+			if data["response"] != nil {
+				metrics := []string{
+					fmt.Sprintf(`doku_llm,environment=%v,applicationName=%v,source=%v,model=%v completionTokens=%v`, data["environment"], data["applicationName"],data["sourceLanguage"], data["model"], data["completionTokens"]),
+					fmt.Sprintf(`doku_llm,environment=%v,applicationName=%v,source=%v,model=%v promptTokens=%v`, data["environment"], data["applicationName"], data["sourceLanguage"], data["model"], data["promptTokens"]),
+					fmt.Sprintf(`doku_llm,environment=%v,applicationName=%v,source=%v,model=%v totalTokens=%v`, data["environment"], data["applicationName"], data["sourceLanguage"], data["model"], data["totalTokens"]),
+					fmt.Sprintf(`doku_llm,environment=%v,applicationName=%v,source=%v,model=%v requestDuration=%v`, data["environment"], data["applicationName"], data["sourceLanguage"], data["model"], data["requestDuration"]),
+					fmt.Sprintf(`doku_llm,environment=%v,applicationName=%v,source=%v,model=%v usageCost=%v`, data["environment"], data["applicationName"], data["sourceLanguage"], data["model"], data["usageCost"]),
+				}
+				metricsBody := strings.Join(metrics, "\n")
 
-		// Populate the streamData only with non-nil fields
-		for _, field := range fields {
-			if value, ok := data[field]; ok && value != nil {
-				streamData[field] = value
-			}
-		}
+				req, err := http.NewRequest("POST", URL, bytes(metricsBody))
+				req.Header.Set("Content-Type", "text/plain")
+				req.Header.Set("Authorization", bearer)
+				client := &http.Client{}
+				resp, err := client.Do(req)
 
-		// Creating the values array
-		timestamp := strconv.FormatInt(time.Now().UnixNano(), 10)
-		response := data["response"]
-		if response == nil {
-			response = "Non Text Generation data collected by Doku"
+				if err != nil {
+					panic(err)
+				}
+				defer resp.Body.Close()
+				
+				logs := []byte(fmt.Sprintf("{\"streams\": [{\"stream\": {\"environment\": \"%v\", \"applicationName\": \"%v\", \"source\": \"%v\", \"model\": \"%v\", \"prompt\": \"%v\" }, \"values\": [[\"%s\", \"%v\"]]}]}",data["environment"], data["applicationName"],data["sourceLanguage"], data["model"], data["prompt"], strconv.FormatInt(time.Now().UnixNano(), 10), data["response"]))
+				http.Post(grafanaLokiPostUrl, "application/json", bytes.NewBuffer(logs))
 		}
-		values := [][]interface{}{{timestamp, response}}
-
-		// Construct the log stream object
-		stream := map[string]interface{}{
-			"stream": streamData,
-			"values": values,
-		}
-
-		// Construct the final logStream structure with the streams array
-		logStream := map[string]interface{}{
-			"streams": []interface{}{stream},
-		}
-
-		// Marshal the logStream into JSON
-		logs, _ := json.MarshalIndent(logStream, "", "  ") // Using Indent for pretty print
-
-		_, err := httpClient.Post(grafanaPostUrl, "application/json", bytes.NewBuffer(logs))
-		if err != nil {
-			log.Error().Err(err).Msg("Error sending data to Grafana Loki")
-		}
-		log.Info().Msg("Data sent to Grafana Loki")
+	}
 
 	case "Datadog":
 		fmt.Println("DataDog Observability Platform")
